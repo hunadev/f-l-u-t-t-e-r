fa3e146e1a613d5b70440cc57861ba5523136799
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-29 17:07:43 +0700

    [DEV] simple interact network

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_app_demo_rain/ui/login_form.dart';
-void main(){
+import 'package:http/http.dart' as http;
+
+void main() async {
+
+  String _data = await getJson();
+  print("HUUHOANG: "+_data);
   runApp(new MaterialApp(
-    title: "the APP title",
-    home: new Login(),
+      title: "the APP title",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("hello app"),
+        ),
+        body: new Center(
+            child: new Text(_data)
+        ),
+      )
   ));
+}
+
+Future<String> getJson() async {
+  String apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
+  http.Response res = await http.get(apiUrl);
+  return json.decode(res.body).toString();
 }
