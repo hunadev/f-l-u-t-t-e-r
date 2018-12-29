54e8cf96fc5d450058d58c56518c25935b63c1f2
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-29 18:35:35 +0700

    [DEV] nested complex json tree,

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   List _data = await getJson();
-  print(" ${_data[99]['title']} ${_data.length}");
   for (int i = 0; i < _data.length; i++) {
-    print("id: ${_data[i]['id']} \t title: ${_data[i]['title']}");
+    print("id: ${_data[i]['address']}");
+    print("street: ${_data[i]['address']['street']}");
+    print("zipcode: ${_data[i]['address']['zipcode']}");
   }
   runApp(new MaterialApp(
       title: "the APP title",
@@ -17,39 +18,12 @@ void main() async {
           title: new Text("hello app"),
         ),
         body: new Center(
-            child: new ListView.builder(
-          padding: const EdgeInsets.all(14.5),
-          itemCount: _data.length,
-          itemBuilder: (BuildContext context, int position) {
-            return new Column(
-              children: <Widget>[
-                new Divider(
-                  height: 5.5,
-                ),
-                new ListTile(
-                  title: new Text(
-                    "${_data[position]['title']}",
-                    style:
-                        new TextStyle(fontSize: 22.00, color: Colors.redAccent),
-                  ),
-                  subtitle: new Text(
-                    "${_data[position]['body']}",
-                    style: new TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  trailing: new CircleAvatar(
-                    child: Text("${_data[position]['body'][0]}"),
-                  ),
-                  onTap:()=>_showOntapMessage(context,_data[position]['title']),
-                )
-              ],
-            );
-          },
-        )),
+            child: Text("HHH")),
       )));
 }
 
 Future<List> getJson() async {
-  String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  String apiUrl = "http://jsonplaceholder.typicode.com/users/";
   http.Response res = await http.get(apiUrl);
   return json.decode(res.body);
 }
