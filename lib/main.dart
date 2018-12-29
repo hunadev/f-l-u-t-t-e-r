9f78856ec5b43d67bb0ea04ff0083cc091bd7271
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-29 20:03:34 +0700

    [DEV] add final class, not happen changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'util/Utils.dart';
+
 void main() async {
   Map _data = await getJson();
 
---
+++ lib/util/Utils.dart
@@ -0,0 +1,2 @@
+final appId = "59a9f4f70952c60ebe2049b6648c65a3";
+final defaultCity = "london";
