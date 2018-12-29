3a247827775f3bb973e6302a8cd857b7ab205493
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-29 17:55:50 +0700

    [DEV] push data to listview with ListView.Builder

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-
   List _data = await getJson();
   print(" ${_data[99]['title']} ${_data.length}");
-  for(int i =0; i< _data.length; i++){
+  for (int i = 0; i < _data.length; i++) {
     print("id: ${_data[i]['id']} \t title: ${_data[i]['title']}");
   }
   runApp(new MaterialApp(
@@ -18,10 +17,35 @@ void main() async {
           title: new Text("hello app"),
         ),
         body: new Center(
-            child: new Text("${_data[0]['body']}")
-        ),
-      )
-  ));
+            child: new ListView.builder(
+          padding: const EdgeInsets.all(14.5),
+          itemCount: _data.length,
+          itemBuilder: (BuildContext context, int position) {
+            return new Column(
+              children: <Widget>[
+                new Divider(
+                  height: 5.5,
+                ),
+                new ListTile(
+                  title: new Text(
+                    "${_data[position]['title']}",
+                    style:
+                        new TextStyle(fontSize: 22.00, color: Colors.redAccent),
+                  ),
+                  subtitle: new Text(
+                    "${_data[position]['body']}",
+                    style: new TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  trailing: new CircleAvatar(
+                    child: Text("${_data[position]['body'][0]}"),
+                  ),
+                  onTap: () => debugPrint("HELLo clicked $position"),
+                )
+              ],
+            );
+          },
+        )),
+      )));
 }
 
 Future<List> getJson() async {
