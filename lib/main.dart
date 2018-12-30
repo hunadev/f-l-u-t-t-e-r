b4e57a2e898c2a423d1b2d8bf418d650e9dfdea5
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-30 17:41:29 +0700

    [DEV] how to start screen? using navigator
    Most apps contain several screens for displaying different types of information. For example, we might have a screen that shows products. Our users could then tap on a product to get more information about it on a new screen.
    
    In Android terms, our screens would be new Activities. In iOS terms, new ViewControllers. In Flutter, screens are just Widgets!
    
    So how do we navigate to new screens? Using the Navigator!

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,49 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() async {
-  Map _data = await getJson();
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstScreen(),
+  ));
+}
 
-  List properties = _data['features'];
-  for (int i = 0; i < properties.length; i++) {
-    print(properties[i]['properties']['mag']);
-  }
-  runApp(new MaterialApp(
-      title: "the APP title",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("hello app"),
-        ),
-        body: new Center(
-            child: new ListView.builder(
-          padding: const EdgeInsets.all(14.5),
-          itemCount: _data.length,
-          itemBuilder: (BuildContext context, int position) {
-            return new Column(
-              children: <Widget>[
-                new Divider(
-                  height: 5.5,
-                ),
-                new ListTile(
-                  title: new Text(
-                    "${properties[position]['properties']['place']}",
-                    style:
-                        new TextStyle(fontSize: 22.00, color: Colors.redAccent),
-                  ),
-                  subtitle: new Text(
-                    "${properties[position]['properties']['title']}",
-                    style: new TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  leading: new CircleAvatar(
-                    child: Text("${properties[position]['properties']['title'][0]}"),
-                  ),
-                  onTap: () =>
-                      _showOntapMessage(context, properties[position]['properties']['title']),
-                )
-              ],
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
             );
           },
-        )),
-      )));
+        ),
+      ),
+    );
+  }
 }
 
-Future<Map> getJson() async {
-  String apiUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-  http.Response res = await http.get(apiUrl);
-  return json.decode(res.body);
-}
-
-void _showOntapMessage(BuildContext context, String msg) {
-  var alert = new AlertDialog(
-    title: Text("MY app"),
-    content: Text(msg),
-    actions: <Widget>[
-      FlatButton(
-        child: Text("OK"),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )
-    ],
-  );
-  showDialog(context: context, builder: (context) => alert);
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
 }
