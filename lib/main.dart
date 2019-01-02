616201b1902827bee98f35bc585e7f8c458e94fd
Huu Hoang <hoangduchuuvn@gmail.com>
2019-01-02 22:56:20 +0700

    [DEV] how to navigate to new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,65 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() async {
-  Map _data = await getJson();
-
-  List properties = _data['features'];
-  for (int i = 0; i < properties.length; i++) {
-    print(properties[i]['properties']['mag']);
-  }
+void main() {
   runApp(new MaterialApp(
-      title: "the APP title",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("hello app"),
-        ),
-        body: new Center(
-            child: new ListView.builder(
-          padding: const EdgeInsets.all(14.5),
-          itemCount: _data.length,
-          itemBuilder: (BuildContext context, int position) {
-            return new Column(
-              children: <Widget>[
-                new Divider(
-                  height: 5.5,
-                ),
-                new ListTile(
-                  title: new Text(
-                    "${properties[position]['properties']['place']}",
-                    style:
-                        new TextStyle(fontSize: 22.00, color: Colors.redAccent),
-                  ),
-                  subtitle: new Text(
-                    "${properties[position]['properties']['title']}",
-                    style: new TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  leading: new CircleAvatar(
-                    child: Text("${properties[position]['properties']['title'][0]}"),
-                  ),
-                  onTap: () =>
-                      _showOntapMessage(context, properties[position]['properties']['title']),
-                )
-              ],
-            );
-          },
-        )),
-      )));
+    title: "APP",
+    home: new Home(),
+  ));
 }
 
-Future<Map> getJson() async {
-  String apiUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-  http.Response res = await http.get(apiUrl);
-  return json.decode(res.body);
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-void _showOntapMessage(BuildContext context, String msg) {
-  var alert = new AlertDialog(
-    title: Text("MY app"),
-    content: Text(msg),
-    actions: <Widget>[
-      FlatButton(
-        child: Text("OK"),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+class _HomeState extends State<Home> {
+  var editext = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("First my screen"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new TextField(
+              controller: editext,
+              decoration: new InputDecoration(labelText: " ENTER NAME HINT"),
+            ),
+          ),
+          new ListTile(
+            title: new RaisedButton(
+                child: Text("Send to next screen"),
+                onPressed: () {
+
+                  var router =  MaterialPageRoute(builder: (context) => NextScreen());
+                  Navigator.push(context, router);
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatefulWidget {
+  @override
+  _NextScreenState createState() => _NextScreenState();
+}
+
+class _NextScreenState extends State<NextScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text("Second title"),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
       )
-    ],
-  );
-  showDialog(context: context, builder: (context) => alert);
+    );
+  }
 }
